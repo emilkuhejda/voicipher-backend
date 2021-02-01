a3915d62c0fd08dbaab65fb1803c6708948091ea
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-01 17:49:31 +0100

    Create audio file controller and empty projects. (#2)

---
+++ src/web/Voicipher.Host/Controllers/V2/AudioFileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Voicipher.Host.Controllers.V2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AudioFileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
