a9f8cbf97c352227e722b574fee667c12f5b0b5b
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-05 18:04:57 +0100

    Implement infrastructure for data access. (#8)

---
+++ src/components/Voicipher.DataAccess/Extensions/DatabaseContextExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Voicipher.DataAccess.Extensions
+{
+    public static class DatabaseContextExtensions
+    {
+        public static void SeedDatabase(this DatabaseContext context)
+        { }
+    }
+}
---
+++ src/components/Voicipher.Domain/Interfaces/Services/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Voicipher.Domain.Interfaces.Services
+{
+    public interface IUnitOfWork
+    {
+        Task SaveAsync(CancellationToken cancellationToken = default);
+    }
+}
