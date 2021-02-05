ff434be639e25bb112db95ef1b641b0635e3fa4e
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-05 18:16:10 +0100

    Infrastructure for CQRS. (#9)

---
+++ src/components/Voicipher.Business/Infrastructure/Query.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Voicipher.Domain.Interfaces.Infrastructure;
+
+namespace Voicipher.Business.Infrastructure
+{
+    public abstract class Query<T, TResult> : IQuery<T, TResult>
+    {
+        protected abstract Task<TResult> Execute(T parameter, ClaimsPrincipal principal, CancellationToken cancellationToken);
+
+        public async Task<TResult> ExecuteAsync(T parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
+        {
+            return await Execute(parameter, principal, cancellationToken);
+        }
+    }
+
+    public abstract class Query<TResult> : IQuery<TResult>
+    {
+        protected abstract Task<TResult> Execute(ClaimsPrincipal principal, CancellationToken cancellationToken);
+
+        public async Task<TResult> ExecuteAsync(ClaimsPrincipal principal, CancellationToken cancellationToken)
+        {
+            return await Execute(principal, cancellationToken);
+        }
+    }
+}
---
+++ src/components/Voicipher.Domain/Interfaces/Infrastructure/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Voicipher.Domain.Interfaces.Infrastructure
+{
+    public interface ICommand<in T, TResult>
+    {
+        Task<TResult> ExecuteAsync(T parameter, ClaimsPrincipal principal, CancellationToken cancellationToken);
+    }
+
+    public interface ICommand<in T>
+    {
+        Task ExecuteAsync(T parameter, ClaimsPrincipal principal, CancellationToken cancellationToken);
+    }
+}
