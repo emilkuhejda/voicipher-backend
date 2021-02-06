9bd868fdf9a2016d82089d42b0b8d0f5cd9e711d
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-06 21:50:18 +0100

    Add StyleCop Analyzer NuGet package. (#15)

---
+++ src/web/Voicipher.Host/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1019:Define accessors for attribute arguments", Justification = "By design.", Scope = "member", Target = "~M:Voicipher.Host.Filters.ApiExceptionFilter.#ctor(Serilog.ILogger)")]
+[assembly: SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "By design.", Scope = "type", Target = "~T:Voicipher.Host.Program")]
