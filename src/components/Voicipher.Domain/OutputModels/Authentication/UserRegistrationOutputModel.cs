b46c3cb657a4c0efd13a4476b1ee62f460057f4c
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-07 12:07:02 +0100

    Generate access token after B2C authorization. (#17)

--- src/components/Voicipher.Domain/OutputModels/Authentication/UserRegistrationOutputModel.cs
+++ src/components/Voicipher.Domain/OutputModels/Authentication/UserRegistrationOutputModel.cs
@@ -1,8 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Voicipher.Domain.Interfaces.Validation;
+using Voicipher.Domain.Validation;
+using ValidationResult = Voicipher.Domain.Validation.ValidationResult;
 
 namespace Voicipher.Domain.OutputModels.Authentication
 {
-    public record UserRegistrationOutputModel
+    public record UserRegistrationOutputModel : IValidatable
     {
         [Required]
         public string Token { get; init; }
@@ -13,5 +17,21 @@ namespace Voicipher.Domain.OutputModels.Authentication
         public IdentityOutputModel Identity { get; init; }
 
         public TimeSpanWrapperOutputModel RemainingTime { get; init; }
+
+        public ValidationResult Validate()
+        {
+            IList<ValidationError> errors = new List<ValidationError>();
+
+            errors.ValidateRequired(Token, nameof(Token), nameof(UserRegistrationOutputModel));
+            errors.ValidateRequired(RefreshToken, nameof(RefreshToken), nameof(UserRegistrationOutputModel));
+            errors.ValidateRequired(Identity, nameof(Identity), nameof(UserRegistrationOutputModel));
+
+            if (Identity != null)
+            {
+                errors.Merge(Identity.Validate());
+            }
+
+            return new ValidationResult(errors);
+        }
     }
 }
