22bb9111df0991301f4d47dff0a8bd1cdba18931
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-07 12:52:48 +0100

    Create administrator entity.

---
+++ src/components/Voicipher.DataAccess/EntitiesConfiguration/AdministratorConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Voicipher.Domain.Models;
+
+namespace Voicipher.DataAccess.EntitiesConfiguration
+{
+    public class AdministratorConfiguration : IEntityTypeConfiguration<Administrator>
+    {
+        public void Configure(EntityTypeBuilder<Administrator> builder)
+        {
+            builder.ToTable("Administrator");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Username).IsRequired();
+            builder.Property(x => x.FirstName).IsRequired();
+            builder.Property(x => x.LastName).IsRequired();
+            builder.Property(x => x.PasswordHash).IsRequired();
+            builder.Property(x => x.PasswordSalt).IsRequired();
+        }
+    }
+}
---
+++ src/components/Voicipher.Domain/Helpers/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Voicipher.Domain.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static (byte[] PasswordHash, byte[] PasswordSalt) CreateHash(string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(password));
+
+            using (var hmac = new HMACSHA512())
+            {
+                return (hmac.ComputeHash(Encoding.UTF8.GetBytes(password)), hmac.Key);
+            }
+        }
+    }
+}
