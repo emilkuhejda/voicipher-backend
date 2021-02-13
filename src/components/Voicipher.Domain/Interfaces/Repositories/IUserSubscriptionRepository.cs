ce00cd331a2a54812419550a85902f440e0fb5a3
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-13 21:41:40 +0100

    Retrieve remaining subscription time and last update. (#31)

---
+++ src/components/Voicipher.Domain/Interfaces/Repositories/IUserSubscriptionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Voicipher.Domain.Models;
+
+namespace Voicipher.Domain.Interfaces.Repositories
+{
+    public interface IUserSubscriptionRepository : IRepository<UserSubscription>
+    {
+        Task<DateTime> GetLastUpdateAsync(Guid userId, CancellationToken cancellationToken);
+
+        Task<TimeSpan> GetRemainingTimeAsync(Guid userId, CancellationToken cancellationToken);
+    }
+}
