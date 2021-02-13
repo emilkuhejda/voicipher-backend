537670ed42140c154e293c72531f1e0365f5792f
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-13 22:50:43 +0100

    Update entiy.

---
+++ src/components/Voicipher.DataAccess/Migrations/20210213214946_UpdateTranscribeItemEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Voicipher.DataAccess.Migrations
+{
+    public partial class UpdateTranscribeItemEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
