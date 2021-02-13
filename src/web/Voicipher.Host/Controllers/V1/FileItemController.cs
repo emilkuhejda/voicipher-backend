6c0db69024825979e9bb0435276d099cde9ff2d3
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-13 19:40:51 +0100

    Update file item controller.

--- src/web/Voicipher.Host/Controllers/V1/FileItemController.cs
+++ src/web/Voicipher.Host/Controllers/V1/FileItemController.cs
@@ -14,10 +14,9 @@ using Voicipher.Host.Utils;
 
 namespace Voicipher.Host.Controllers.V1
 {
-    [ApiVersion("1.0")]
-    [ApiExplorerSettings(GroupName = "v1")]
-    [Route("api/v{version:apiVersion}/files")]
+    [ApiVersion("1")]
     [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/files")]
     [Authorize(Policy = nameof(VoicipherPolicy.User))]
     [ApiController]
     public class FileItemController : ControllerBase
@@ -29,15 +28,54 @@ namespace Voicipher.Host.Controllers.V1
             _createAudioFileCommand = createAudioFileCommand;
         }
 
+        [HttpGet]
+        // [ProducesResponseType(typeof(IEnumerable<FileItemDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "GetFileItems")]
+        public ActionResult Get(DateTime updatedAfter, Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("deleted")]
+        // [ProducesResponseType(typeof(IEnumerable<Guid>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "GetDeletedFileItemIds")]
+        public IActionResult GetDeletedFileItemIds(DateTime updatedAfter, Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("temporary-deleted")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult GetTemporaryDeletedFileItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{fileItemId}")]
+        // [ProducesResponseType(typeof(FileItemDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "GetFileItem")]
+        public IActionResult Get(Guid fileItemId)
+        {
+            throw new NotImplementedException();
+        }
+
         [HttpPost("create")]
-        [AllowAnonymous]
         [ProducesResponseType(typeof(AudioFileOutputModel), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ErrorCode), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [SwaggerOperation(OperationId = "CreateAudioFile")]
-        public async Task<IActionResult> CreateAudioFile(string name, string language, string fileName, DateTime dateCreated, Guid applicationId, CancellationToken cancellationToken)
+        [SwaggerOperation(OperationId = "CreateFileItem")]
+        public async Task<IActionResult> CreateFileItem(string name, string language, string fileName, DateTime dateCreated, Guid applicationId, CancellationToken cancellationToken)
         {
             var createAudioFilePayload = new CreateAudioFilePayload
             {
@@ -53,6 +91,84 @@ namespace Voicipher.Host.Controllers.V1
                 throw new OperationErrorException(ErrorCode.EC601);
 
             return Ok(commandResult.Value);
+        }
+
+        [HttpPost("upload")]
+        [Consumes("multipart/form-data")]
+        // [ProducesResponseType(typeof(FileItemDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorCode), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "UploadFileItem")]
+        [RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
+        [RequestSizeLimit(int.MaxValue)]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Upload(string name, string language, string fileName, DateTime dateCreated, Guid applicationId, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("update")]
+        // [ProducesResponseType(typeof(FileItemDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorCode), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "UpdateFileItem")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Update([FromForm] object updateFileItemModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("delete")]
+        // [ProducesResponseType(typeof(OkDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorCode), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "DeleteFileItem")]
+        public IActionResult Delete(Guid fileItemId, Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("delete-all")]
+        // [ProducesResponseType(typeof(OkDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "DeleteAllFileItems")]
+        public IActionResult DeleteAll(object fileItems, Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("permanent-delete-all")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult PermanentDeleteAll(object fileItemIds, Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("restore-all")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult RestoreAll(object fileItemIds, Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("transcribe")]
+        // [ProducesResponseType(typeof(OkDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorCode), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "TranscribeFileItem")]
+        public IActionResult Transcribe(Guid fileItemId, string language, Guid applicationId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
