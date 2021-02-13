fe56b2fe6d0c5bb69822a8367a6c74c460233384
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-13 20:01:37 +0100

    Update controller.

--- src/web/Voicipher.Host/Controllers/V1/UserController.cs
+++ src/web/Voicipher.Host/Controllers/V1/UserController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using Voicipher.Domain.Enums;
 using Voicipher.Domain.InputModels.Authentication;
 using Voicipher.Domain.Interfaces.Commands.Authentication;
 using Voicipher.Domain.OutputModels;
@@ -15,10 +16,9 @@ using Voicipher.Host.Utils;
 namespace Voicipher.Host.Controllers.V1
 {
     [ApiVersion("1")]
-    [Route("api/v{version:apiVersion}/users")]
     [Produces("application/json")]
-    [Authorize(Policy = nameof(VoicipherPolicy.AzureAd))]
-    [ApiExplorerSettings(GroupName = "v1")]
+    [Route("api/v{version:apiVersion}/users")]
+    [Authorize]
     [ApiController]
     public class UserController : ControllerBase
     {
@@ -33,9 +33,24 @@ namespace Voicipher.Host.Controllers.V1
             _mapper = mapper;
         }
 
+        [HttpPut("update")]
+        [Authorize(Policy = nameof(VoicipherPolicy.User))]
+        // [ProducesResponseType(typeof(IdentityDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "UpdateUser")]
+        public IActionResult UpdateUser([FromBody] object updateUserModel)
+        {
+            throw new NotImplementedException();
+        }
+
         [HttpPost("/api/b2c/v{version:apiVersion}/users/register")]
+        [Authorize(Policy = nameof(VoicipherPolicy.AzureAd))]
         [ProducesResponseType(typeof(UserRegistrationOutputModel), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [SwaggerOperation(OperationId = "RegisterUser")]
         public async Task<IActionResult> Register([FromBody] UserRegistrationInputModel registrationUserRegistrationModel, CancellationToken cancellationToken)
@@ -45,6 +60,32 @@ namespace Voicipher.Host.Controllers.V1
                 return BadRequest(_mapper.Value.Map<ErrorResultOutputModel>(commandResult));
 
             return Ok(commandResult.Value);
+        }
+
+        [HttpPut("update-language")]
+        [Authorize(Policy = nameof(VoicipherPolicy.User))]
+        // [ProducesResponseType(typeof(OkDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorCode), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "UpdateLanguage")]
+        public IActionResult UpdateLanguage(Guid installationId, int language)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Authorize(Policy = nameof(VoicipherPolicy.User))]
+        // [ProducesResponseType(typeof(OkDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(OperationId = "DeleteUser")]
+        public IActionResult DeleteUser(string email)
+        {
+            throw new NotImplementedException();
         }
     }
 }
