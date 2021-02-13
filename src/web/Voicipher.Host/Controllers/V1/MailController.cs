01ef469acbdf4880160a32fc10ca9b1b82652687
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-13 19:50:13 +0100

    Update controllers.

---
+++ src/web/Voicipher.Host/Controllers/V1/MailController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Voicipher.Host.Utils;
+
+namespace Voicipher.Host.Controllers.V1
+{
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/mail")]
+    [Authorize(Policy = nameof(VoicipherPolicy.User))]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail([FromBody] object mailModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
