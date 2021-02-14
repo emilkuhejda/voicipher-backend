7d3f30f2aebd8c75527ab4cd401936e9f60bb26d
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-14 11:28:21 +0100

    Create update command.

---
+++ src/components/Voicipher.Business/Commands/Audio/UpdateAudioFileCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Voicipher.Business.Infrastructure;
+using Voicipher.Domain.Infrastructure;
+using Voicipher.Domain.InputModels.Audio;
+using Voicipher.Domain.Interfaces.Commands.Audio;
+using Voicipher.Domain.OutputModels.Audio;
+
+namespace Voicipher.Business.Commands.Audio
+{
+    public class UpdateAudioFileCommand : Command<UpdateAudioFileInputModel, CommandResult<FileItemOutputModel>>, IUpdateAudioFileCommand
+    {
+        protected override async Task<CommandResult<FileItemOutputModel>> Execute(UpdateAudioFileInputModel parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+
+            return new CommandResult<FileItemOutputModel>(new FileItemOutputModel());
+        }
+    }
+}
---
+++ src/components/Voicipher.Domain/InputModels/Audio/UpdateAudioFileInputModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Voicipher.Domain.Interfaces.Validation;
+using Voicipher.Domain.Validation;
+using ValidationResult = Voicipher.Domain.Validation.ValidationResult;
+
+namespace Voicipher.Domain.InputModels.Audio
+{
+    public record UpdateAudioFileInputModel : IValidatable
+    {
+        [Required]
+        [BindProperty(Name = "FileItemId")]
+        public Guid AudioFileId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        public Guid ApplicationId { get; set; }
+
+        public ValidationResult Validate()
+        {
+            IList<ValidationError> errors = new List<ValidationError>();
+
+            errors.ValidateGuid(AudioFileId, nameof(AudioFileId));
+            errors.ValidateRequired(Name, nameof(Name));
+            errors.ValidateRequired(Language, nameof(Language));
+            errors.ValidateGuid(ApplicationId, nameof(ApplicationId));
+
+            return new ValidationResult(errors);
+        }
+    }
+}
