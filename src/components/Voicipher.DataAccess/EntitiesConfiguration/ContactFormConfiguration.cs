f04a6439f554b591e0da92d54ef8915582d6a60e
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-14 19:15:09 +0100

    Implementation of the endpoint for storing contact forms. (#40)

---
+++ src/components/Voicipher.DataAccess/EntitiesConfiguration/ContactFormConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Voicipher.Domain.Models;
+
+namespace Voicipher.DataAccess.EntitiesConfiguration
+{
+    public class ContactFormConfiguration : IEntityTypeConfiguration<ContactForm>
+    {
+        public void Configure(EntityTypeBuilder<ContactForm> builder)
+        {
+            builder.ToTable("ContactForm");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(150).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(150).IsRequired();
+            builder.Property(x => x.Message).IsRequired();
+            builder.Property(x => x.DateCreatedUtc).IsRequired();
+        }
+    }
+}
--- src/web/Voicipher.Host/Controllers/V1/ContactFormController.cs
+++ src/web/Voicipher.Host/Controllers/V1/ContactFormController.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using Voicipher.Domain.Enums;
+using Voicipher.Domain.Exceptions;
+using Voicipher.Domain.InputModels;
+using Voicipher.Domain.Interfaces.Commands;
+using Voicipher.Domain.OutputModels;
 
 namespace Voicipher.Host.Controllers.V1
 {
@@ -12,13 +19,25 @@ namespace Voicipher.Host.Controllers.V1
     [ApiController]
     public class ContactFormController : ControllerBase
     {
+        private readonly Lazy<IContactFormCommand> _contactFormCommand;
+
+        public ContactFormController(Lazy<IContactFormCommand> contactFormCommand)
+        {
+            _contactFormCommand = contactFormCommand;
+        }
+
         [HttpPost("create")]
-        // [ProducesResponseType(typeof(OkDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(OkOutputModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [SwaggerOperation(OperationId = "CreateContactForm")]
-        public IActionResult Create([FromBody] object contactFormModel)
+        public async Task<IActionResult> Create([FromBody] ContactFormInputModel contactFormInputModel, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var commandResult = await _contactFormCommand.Value.ExecuteAsync(contactFormInputModel, HttpContext.User, cancellationToken);
+            if (!commandResult.IsSuccess)
+                throw new OperationErrorException(ErrorCode.EC601);
+
+            return Ok(commandResult.Value);
         }
     }
 }
