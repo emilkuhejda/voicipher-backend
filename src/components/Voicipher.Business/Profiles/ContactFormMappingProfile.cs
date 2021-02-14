7a589b80b145e977f854f0227905003c9350cc5b
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-14 19:13:23 +0100

    Store contact form.

---
+++ src/components/Voicipher.Business/Profiles/ContactFormMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper;
+using Voicipher.Domain.InputModels;
+using Voicipher.Domain.Models;
+
+namespace Voicipher.Business.Profiles
+{
+    public class ContactFormMappingProfile : Profile
+    {
+        public ContactFormMappingProfile()
+        {
+            CreateMap<ContactFormInputModel, ContactForm>()
+                .ForMember(
+                    c => c.Id,
+                    opt => opt.MapFrom(x => Guid.NewGuid()))
+                .ForMember(
+                    c => c.Name,
+                    opt => opt.MapFrom(x => x.Name))
+                .ForMember(
+                    c => c.Email,
+                    opt => opt.MapFrom(x => x.Email))
+                .ForMember(
+                    c => c.Message,
+                    opt => opt.MapFrom(x => x.Message))
+                .ForMember(
+                    c => c.DateCreatedUtc,
+                    opt => opt.MapFrom(x => DateTime.UtcNow));
+        }
+    }
+}
