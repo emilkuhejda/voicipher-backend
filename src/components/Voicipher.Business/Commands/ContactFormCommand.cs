e227cc96aa3c0e9b1f728a4e56dacabd24782d2e
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-14 19:01:46 +0100

    Create contact form entity.

---
+++ src/components/Voicipher.Business/Commands/ContactFormCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Voicipher.Business.Infrastructure;
+using Voicipher.Domain.Infrastructure;
+using Voicipher.Domain.InputModels;
+using Voicipher.Domain.Interfaces.Commands;
+using Voicipher.Domain.OutputModels;
+
+namespace Voicipher.Business.Commands
+{
+    public class ContactFormCommand : Command<ContactFormInputModel, CommandResult<OkOutputModel>>, IContactFormCommand
+    {
+        protected override Task<CommandResult<OkOutputModel>> Execute(ContactFormInputModel parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/components/Voicipher.Domain/Models/ContactForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Voicipher.Domain.Interfaces.Validation;
+using Voicipher.Domain.Validation;
+
+namespace Voicipher.Domain.Models
+{
+    public class ContactForm : EntityBase, IValidatable
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime DateCreatedUtc { get; set; }
+
+        public ValidationResult Validate()
+        {
+            IList<ValidationError> errors = new List<ValidationError>();
+
+            errors.ValidateGuid(Id, nameof(Id));
+            errors.ValidateRequired(Name, nameof(Name));
+            errors.ValidateMaxLength(Name, nameof(Name), 150);
+            errors.ValidateRequired(Email, nameof(Email));
+            errors.ValidateMaxLength(Email, nameof(Email), 150);
+            errors.ValidateRequired(Message, nameof(Message));
+            errors.ValidateDateTime(DateCreatedUtc, nameof(DateCreatedUtc));
+
+            return new ValidationResult(errors);
+        }
+    }
+}
