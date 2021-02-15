efab020726e1d7caacdabe99576b67a9080f767a
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-15 09:19:23 +0100

    Create background service for sending emails. (#43)

---
+++ src/components/Voicipher.Business/Channels/MailProcessingChannel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Voicipher.Domain.Interfaces.Channels;
+using Voicipher.Domain.Models;
+
+namespace Voicipher.Business.Channels
+{
+    public class MailProcessingChannel : IMailProcessingChannel
+    {
+        private readonly Channel<MailData> _channel;
+
+        public MailProcessingChannel()
+        {
+            _channel = Channel.CreateUnbounded<MailData>();
+        }
+
+        public IAsyncEnumerable<MailData> ReadAllAsync(CancellationToken cancellationToken = default) => _channel.Reader.ReadAllAsync(cancellationToken);
+
+        public async Task<bool> AddFileAsync(MailData mailData, CancellationToken cancellationToken = default)
+        {
+            while (await _channel.Writer.WaitToWriteAsync(cancellationToken) && !cancellationToken.IsCancellationRequested)
+            {
+                if (_channel.Writer.TryWrite(mailData))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
