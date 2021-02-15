7d85edde76b7673de57f1aaa7719f15f2f39090f
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-15 17:33:23 +0100

    Update transcribe item.

--- src/components/Voicipher.Business/Commands/UpdateUserTranscriptCommand.cs
+++ src/components/Voicipher.Business/Commands/UpdateUserTranscriptCommand.cs
@@ -1,8 +1,11 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using Serilog;
 using Voicipher.Business.Infrastructure;
+using Voicipher.Domain.Enums;
+using Voicipher.Domain.Exceptions;
 using Voicipher.Domain.Infrastructure;
 using Voicipher.Domain.InputModels;
 using Voicipher.Domain.Interfaces.Commands;
@@ -24,8 +27,32 @@ namespace Voicipher.Business.Commands
             _logger = logger.ForContext<UpdateUserTranscriptCommand>();
         }
 
-        protected override Task<CommandResult<OkOutputModel>> Execute(UpdateUserTranscriptInputModel parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
+        protected override async Task<CommandResult<OkOutputModel>> Execute(UpdateUserTranscriptInputModel parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
         {
+            if (!parameter.Validate().IsValid)
+            {
+                _logger.Error("Invalid input data.");
+
+                throw new OperationErrorException(ErrorCode.EC600);
+            }
+
+            var transcribeItem = await _transcribeItemRepository.GetAsync(parameter.TranscribeItemId, cancellationToken);
+            if (transcribeItem == null)
+            {
+                _logger.Error($"Transcribe item '{parameter.TranscribeItemId}' not found.");
+
+                throw new OperationErrorException(ErrorCode.EC101);
+            }
+
+            transcribeItem.UserTranscript = parameter.Transcript;
+            transcribeItem.ApplicationId = parameter.ApplicationId;
+            transcribeItem.DateUpdatedUtc = DateTime.UtcNow;
+
+            await _transcribeItemRepository.SaveAsync(cancellationToken);
+
+            _logger.Information($"Transcribe item '{parameter.TranscribeItemId}' was updated.");
+
+            return new CommandResult<OkOutputModel>(new OkOutputModel());
         }
     }
 }
