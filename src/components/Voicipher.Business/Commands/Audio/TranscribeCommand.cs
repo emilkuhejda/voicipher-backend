89ed57fa6cf340f1d836ea90d77db6a25053e735
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-16 09:38:02 +0100

    Check audio file before recognition.

--- src/components/Voicipher.Business/Commands/Audio/TranscribeCommand.cs
+++ src/components/Voicipher.Business/Commands/Audio/TranscribeCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using Serilog;
@@ -8,6 +9,7 @@ using Voicipher.Domain.Enums;
 using Voicipher.Domain.Exceptions;
 using Voicipher.Domain.Infrastructure;
 using Voicipher.Domain.Interfaces.Commands.Audio;
+using Voicipher.Domain.Interfaces.Repositories;
 using Voicipher.Domain.OutputModels;
 using Voicipher.Domain.Payloads.Audio;
 using Voicipher.Domain.Validation;
@@ -16,31 +18,61 @@ namespace Voicipher.Business.Commands.Audio
 {
     public class TranscribeCommand : Command<TranscribePayload, CommandResult<OkOutputModel>>, ITranscribeCommand
     {
+        private readonly IAudioFileRepository _audioFileRepository;
         private readonly ILogger _logger;
 
-        public TranscribeCommand(ILogger logger)
+        public TranscribeCommand(
+            IAudioFileRepository audioFileRepository,
+            ILogger logger)
         {
+            _audioFileRepository = audioFileRepository;
             _logger = logger.ForContext<TranscribeCommand>();
         }
 
         protected override async Task<CommandResult<OkOutputModel>> Execute(TranscribePayload parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
         {
+            var userId = principal.GetNameIdentifier();
+            _logger.Information($"Start validation before transcription. User ID = {userId}, Audio file ID = {parameter.AudioFileId}");
+
             var validationResult = parameter.Validate();
             if (!validationResult.IsValid)
             {
-                if (validationResult.Errors.ContainsError(nameof(CreateAudioFilePayload.Language), ValidationErrorCodes.NotSupportedLanguage))
+                if (validationResult.Errors.ContainsError(nameof(TranscribePayload.Language), ValidationErrorCodes.NotSupportedLanguage))
                 {
                     _logger.Error($"Language '{parameter.Language}' is not supported.");
-
                     throw new OperationErrorException(ErrorCode.EC200);
                 }
 
                 _logger.Error("Invalid input data.");
-
                 throw new OperationErrorException(ErrorCode.EC600);
             }
 
-            await Task.CompletedTask;
+            var audioFile = await _audioFileRepository.GetAsync(userId, parameter.AudioFileId, cancellationToken);
+            if (audioFile == null)
+            {
+                _logger.Error($"Audio file '{parameter.AudioFileId}' not exists");
+                throw new OperationErrorException(ErrorCode.EC101);
+            }
+
+            if (audioFile.UploadStatus != UploadStatus.Completed)
+            {
+                _logger.Error($"Audio file source '{parameter.AudioFileId}' is not uploaded. Uploaded state is {audioFile.UploadStatus}");
+                throw new OperationErrorException(ErrorCode.EC104);
+            }
+
+            if (audioFile.RecognitionState != RecognitionState.None)
+            {
+                _logger.Error($"Audio file '{parameter.AudioFileId}' is in the wrong recognition state. Recognition state is {audioFile.RecognitionState}.");
+                throw new OperationErrorException(ErrorCode.EC103);
+            }
+
+            // can run recognition
+            var canRunRecognition = true;
+
+            audioFile.ApplicationId = parameter.ApplicationId;
+            audioFile.Language = parameter.Language;
+            audioFile.DateUpdatedUtc = DateTime.UtcNow;
+            await _audioFileRepository.SaveAsync(cancellationToken);
 
             return new CommandResult<OkOutputModel>(new OkOutputModel());
         }
