5c42d10f847663c276d4ecc7cb914f3d2e79185d
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-16 10:01:04 +0100

    Check subscription minutes.

--- src/components/Voicipher.Business/Commands/Transcription/CanRunRecognitionCommand.cs
+++ src/components/Voicipher.Business/Commands/Transcription/CanRunRecognitionCommand.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Voicipher.Business.Infrastructure;
 using Voicipher.Domain.Infrastructure;
 using Voicipher.Domain.Interfaces.Commands.Transcription;
+using Voicipher.Domain.Interfaces.Repositories;
 using Voicipher.Domain.Payloads;
 using Voicipher.Domain.Validation;
 
@@ -11,10 +12,20 @@ namespace Voicipher.Business.Commands.Transcription
 {
     public class CanRunRecognitionCommand : Command<CanRunRecognitionPayload, CommandResult>, ICanRunRecognitionCommand
     {
+        private readonly ICurrentUserSubscriptionRepository _currentUserSubscriptionRepository;
+
+        public CanRunRecognitionCommand(ICurrentUserSubscriptionRepository currentUserSubscriptionRepository)
+        {
+            _currentUserSubscriptionRepository = currentUserSubscriptionRepository;
+        }
+
         protected override async Task<CommandResult> Execute(CanRunRecognitionPayload parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
         {
-            await Task.CompletedTask;
-            return new CommandResult(new OperationError(string.Empty));
+            var subscriptionRemainingTime = await _currentUserSubscriptionRepository.GetRemainingTimeAsync(parameter.UserId, cancellationToken);
+            if (subscriptionRemainingTime.TotalSeconds < 1)
+                return new CommandResult(new OperationError(ValidationErrorCodes.NotEnoughSubscriptionTime));
+
+            return new CommandResult();
         }
     }
 }
