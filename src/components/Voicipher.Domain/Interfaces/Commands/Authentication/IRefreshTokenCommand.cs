345c9845a5d05081a4447578733e0cd0f7f380a2
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-16 08:49:43 +0100

    Implementation of the endpoint for refreshing token. (#50)

---
+++ src/components/Voicipher.Domain/Interfaces/Commands/Authentication/IRefreshTokenCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Voicipher.Domain.Infrastructure;
+using Voicipher.Domain.Interfaces.Infrastructure;
+
+namespace Voicipher.Domain.Interfaces.Commands.Authentication
+{
+    public interface IRefreshTokenCommand : ICommand<TimeSpan, CommandResult<string>>
+    {
+    }
+}
--- src/components/Voicipher.Domain/Interfaces/Repositories/IUserRepository.cs
+++ src/components/Voicipher.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -8,5 +8,7 @@ namespace Voicipher.Domain.Interfaces.Repositories
     public interface IUserRepository : IRepository<User>
     {
         Task<User> GetByEmailAsync(Guid userId, string email, CancellationToken cancellationToken);
+
+        Task<bool> ExistsAsync(Guid userId, CancellationToken cancellationToken);
     }
 }
