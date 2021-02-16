94ff2f3f4da25558d5b2ed767780ba469f329c94
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-16 08:55:14 +0100

    Expose OpenId configuration settings. (#51)

---
+++ src/web/Voicipher.Host/Controllers/V1/MetaDataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Voicipher.Host.Controllers.V1
+{
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/meta-data")]
+    [ApiController]
+    public class MetaDataController : ControllerBase
+    {
+        [HttpGet(".well-known/openid-configuration")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [SwaggerOperation(OperationId = "GetOpenIdConfiguration")]
+        public IActionResult GetOpenIdConfiguration()
+        {
+            return new OkObjectResult(new
+            {
+                issuer = "https://appleid.apple.com",
+                authorization_endpoint = "https://appleid.apple.com/auth/authorize",
+                token_endpoint = "https://appleid.apple.com/auth/token",
+                jwks_uri = "https://appleid.apple.com/auth/keys",
+                id_token_signing_alg_values_supported = new[] { "RS256" }
+            });
+        }
+    }
+}
