11ae4bf261019c420308e75a0f02a9949f7ae2a5
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-16 13:18:18 +0100

    Create background job.

--- src/components/Voicipher.Business/Commands/Job/CreateBackgroundJobCommand.cs
+++ src/components/Voicipher.Business/Commands/Job/CreateBackgroundJobCommand.cs
@@ -1,9 +1,12 @@
 ﻿using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using Serilog;
 using Voicipher.Business.Infrastructure;
 using Voicipher.Domain.Infrastructure;
 using Voicipher.Domain.Interfaces.Commands.Job;
+using Voicipher.Domain.Interfaces.Repositories;
 using Voicipher.Domain.Models;
 using Voicipher.Domain.Payloads.Job;
 
@@ -11,10 +14,29 @@ namespace Voicipher.Business.Commands.Job
 {
     public class CreateBackgroundJobCommand : Command<CreateBackgroundJobPayload, CommandResult<BackgroundJob>>, ICreateBackgroundJobCommand
     {
+        private readonly IBackgroundJobRepository _backgroundJobRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public CreateBackgroundJobCommand(
+            IBackgroundJobRepository backgroundJobRepository,
+            IMapper mapper,
+            ILogger logger)
+        {
+            _backgroundJobRepository = backgroundJobRepository;
+            _mapper = mapper;
+            _logger = logger.ForContext<CreateBackgroundJobCommand>();
+        }
+
         protected override async Task<CommandResult<BackgroundJob>> Execute(CreateBackgroundJobPayload parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
         {
-            await Task.CompletedTask;
-            return new CommandResult<BackgroundJob>(new BackgroundJob());
+            var backgroundJob = _mapper.Map<BackgroundJob>(parameter);
+            await _backgroundJobRepository.AddAsync(backgroundJob);
+            await _backgroundJobRepository.SaveAsync(cancellationToken);
+
+            _logger.Information($"Background job {backgroundJob.Id} was created");
+
+            return new CommandResult<BackgroundJob>(backgroundJob);
         }
     }
 }
