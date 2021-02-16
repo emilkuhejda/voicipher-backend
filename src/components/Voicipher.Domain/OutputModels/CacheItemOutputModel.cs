8025d9dae7f2d0f41f719f0925b4822bba387786
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-16 09:22:22 +0100

    Return empty cache item from cache endpoint. (#52)

---
+++ src/components/Voicipher.Domain/OutputModels/CacheItemOutputModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Voicipher.Domain.Enums;
+
+namespace Voicipher.Domain.OutputModels
+{
+    public record EmptyCacheItemOutputModel : CacheItemOutputModel
+    {
+        public EmptyCacheItemOutputModel()
+            : base(Guid.Empty, RecognitionState.None, 0)
+        {
+        }
+    }
+
+    public record CacheItemOutputModel
+    {
+        public CacheItemOutputModel(Guid fileItemId, RecognitionState recognitionState, double percentageDone)
+        {
+            FileItemId = fileItemId;
+            RecognitionState = recognitionState;
+            PercentageDone = percentageDone;
+        }
+
+        public Guid FileItemId { get; }
+
+        public RecognitionState RecognitionState { get; }
+
+        public double PercentageDone { get; }
+    }
+}
