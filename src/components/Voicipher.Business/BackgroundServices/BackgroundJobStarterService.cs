560410e44382eb354be4bc32707c3188232429ba
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-17 22:29:22 +0100

    Update background service.

--- src/components/Voicipher.Business/BackgroundServices/BackgroundJobStarterService.cs
+++ src/components/Voicipher.Business/BackgroundServices/BackgroundJobStarterService.cs
@@ -1,19 +1,29 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Autofac.Features.Indexed;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Voicipher.Domain.Enums;
 using Voicipher.Domain.Interfaces.Repositories;
+using Voicipher.Domain.Interfaces.Services;
 
 namespace Voicipher.Business.BackgroundServices
 {
     public class BackgroundJobStarterService : BackgroundService
     {
+        private readonly IServiceProvider _serviceProvider;
         private readonly IBackgroundJobRepository _backgroundJobRepository;
         private readonly ILogger _logger;
 
-        public BackgroundJobStarterService(IBackgroundJobRepository backgroundJobRepository, ILogger logger)
+        public BackgroundJobStarterService(
+            IServiceProvider serviceProvider,
+            IBackgroundJobRepository backgroundJobRepository,
+            ILogger logger)
         {
+            _serviceProvider = serviceProvider;
             _backgroundJobRepository = backgroundJobRepository;
             _logger = logger.ForContext<BackgroundJobStarterService>();
         }
@@ -26,6 +36,12 @@ namespace Voicipher.Business.BackgroundServices
             if (jobs.Any())
             {
                 _logger.Information($"{jobs.Length} background jobs for restart");
+            }
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var diskStorage = scope.ServiceProvider.GetRequiredService<IIndex<StorageLocation, IDiskStorage>>()[StorageLocation.Audio];
+                diskStorage.RemoveTemporaryFolder();
             }
         }
     }
