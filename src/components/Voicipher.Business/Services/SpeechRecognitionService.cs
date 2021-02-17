0afc22d9a3b9dd1c886750914e8577c5674d13bb
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-17 12:42:59 +0100

    Create speech recognition service.

---
+++ src/components/Voicipher.Business/Services/SpeechRecognitionService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Voicipher.Domain.Interfaces.Services;
+
+namespace Voicipher.Business.Services
+{
+    public class SpeechRecognitionService : ISpeechRecognitionService
+    {
+        public Task RecognizeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/components/Voicipher.Domain/Interfaces/Services/ISpeechRecognitionService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Voicipher.Domain.Interfaces.Services
+{
+    public interface ISpeechRecognitionService
+    {
+        Task RecognizeAsync();
+    }
+}
