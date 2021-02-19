68dfbcbadbd045863aeac78ef16e612376397ec5
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-19 12:48:39 +0100

    Return contact forms.

--- src/web/Voicipher.Host/Controllers/ControlPanel/ContactFormController.cs
+++ src/web/Voicipher.Host/Controllers/ControlPanel/ContactFormController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Voicipher.Domain.Interfaces.Repositories;
+using Voicipher.Domain.Models;
 using Voicipher.Host.Utils;
 
 namespace Voicipher.Host.Controllers.ControlPanel
@@ -14,16 +18,41 @@ namespace Voicipher.Host.Controllers.ControlPanel
     [ApiController]
     public class ContactFormController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
+        private readonly Lazy<IContactFormRepository> _contactFormRepository;
+
+        public ContactFormController(Lazy<IContactFormRepository> contactFormRepository)
         {
-            throw new NotImplementedException();
+            _contactFormRepository = contactFormRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+        {
+            var contactForms = await _contactFormRepository.Value.GetAllAsync(cancellationToken);
+            var outputModels = contactForms.Select(MapContactForm).ToArray();
+
+            return Ok(outputModels);
         }
 
         [HttpGet("{contactFormId}")]
-        public async Task<IActionResult> Get(Guid contactFormId)
+        public async Task<IActionResult> Get(Guid contactFormId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var contactForm = await _contactFormRepository.Value.GetAsync(contactFormId, cancellationToken);
+            var outputModel = MapContactForm(contactForm);
+
+            return Ok(outputModel);
+        }
+
+        private object MapContactForm(ContactForm contactForm)
+        {
+            return new
+            {
+                Id = contactForm.Id,
+                Name = contactForm.Name,
+                Email = contactForm.Email,
+                Message = contactForm.Message,
+                DateCreatedUtc = contactForm.DateCreatedUtc
+            };
         }
     }
 }
