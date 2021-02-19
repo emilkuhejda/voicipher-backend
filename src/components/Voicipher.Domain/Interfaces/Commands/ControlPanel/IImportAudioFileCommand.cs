8701721368c9800bbc7b0c9114b1f1a3b956704a
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-19 07:21:08 +0100

    Create audio file importer.

---
+++ src/components/Voicipher.Domain/Interfaces/Commands/ControlPanel/IImportAudioFileCommand.cs
@@ -0,0 +1,10 @@
+﻿using Voicipher.Domain.Infrastructure;
+using Voicipher.Domain.Interfaces.Infrastructure;
+using Voicipher.Domain.Payloads.ControlPanel;
+
+namespace Voicipher.Domain.Interfaces.Commands.ControlPanel
+{
+    public interface IImportAudioFileCommand : ICommand<ImportAudioFilePayload, CommandResult<int>>
+    {
+    }
+}
