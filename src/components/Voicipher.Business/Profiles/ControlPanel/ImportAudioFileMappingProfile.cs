bc407396ab9c36c1700004424108ccf4e988dbda
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-19 07:22:16 +0100

    Create audio file importer. (#63)

---
+++ src/components/Voicipher.Business/Profiles/ControlPanel/ImportAudioFileMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Voicipher.Domain.InputModels.ControlPanel;
+using Voicipher.Domain.Payloads.ControlPanel;
+
+namespace Voicipher.Business.Profiles.ControlPanel
+{
+    public class ImportAudioFileMappingProfile : Profile
+    {
+        public ImportAudioFileMappingProfile()
+        {
+            CreateMap<ImportAudioFileInputModel, ImportAudioFilePayload>()
+                .ForMember(
+                    i => i.UsersJsonPath,
+                    opt => opt.MapFrom(x => x.UsersJsonPath))
+                .ForMember(
+                    i => i.SubscriptionsJsonPath,
+                    opt => opt.MapFrom(x => x.SubscriptionsJsonPath))
+                .ForMember(
+                    i => i.AlternativesJsonPath,
+                    opt => opt.MapFrom(x => x.AlternativesJsonPath))
+                .ForMember(
+                    i => i.UploadsDirectoryPath,
+                    opt => opt.MapFrom(x => x.UploadsDirectoryPath));
+        }
+    }
+}
---
+++ src/components/Voicipher.Domain/InputModels/ControlPanel/ImportAudioFileInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Voicipher.Domain.InputModels.ControlPanel
+{
+    public record ImportAudioFileInputModel
+    {
+        public string UsersJsonPath { get; init; }
+
+        public string SubscriptionsJsonPath { get; init; }
+
+        public string AlternativesJsonPath { get; init; }
+
+        public string UploadsDirectoryPath { get; init; }
+    }
+}
