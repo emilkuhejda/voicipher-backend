9b535d8ae2c39f8130e30f6a32354034d19e5dcc
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-19 08:06:18 +0100

    Update configuration settings for production. (#65)

---
+++ src/components/Voicipher.Common/Utils/StringGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Voicipher.Common.Utils
+{
+    public static class StringGenerator
+    {
+        public static string Generate(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]\\/?><";
+
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
