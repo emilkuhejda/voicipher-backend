f60c7ce52d849f711c4f818a5447ca49302f9501
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-19 12:26:16 +0100

    Return user subscriptions.

---
+++ src/components/Voicipher.Business/Profiles/ControlPanel/ModifySubscriptionTimeMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AutoMapper;
+using Voicipher.Domain.InputModels.ControlPanel;
+using Voicipher.Domain.Payloads;
+
+namespace Voicipher.Business.Profiles.ControlPanel
+{
+    public class ModifySubscriptionTimeMappingProfile : Profile
+    {
+        public ModifySubscriptionTimeMappingProfile()
+        {
+            CreateMap<CreateSubscriptionInputModel, ModifySubscriptionTimePayload>()
+                .ForMember(
+                    m => m.UserId,
+                    opt => opt.MapFrom(x => x.UserId))
+                .ForMember(
+                    m => m.ApplicationId,
+                    opt => opt.MapFrom(x => x.ApplicationId))
+                .ForMember(
+                    m => m.Time,
+                    opt => opt.MapFrom(x => TimeSpan.FromSeconds(x.Seconds)))
+                .ForMember(
+                    m => m.Operation,
+                    opt => opt.MapFrom(x => x.Operation));
+        }
+    }
+}
--- src/web/Voicipher.Host/Controllers/ControlPanel/UserSubscriptionController.cs
+++ src/web/Voicipher.Host/Controllers/ControlPanel/UserSubscriptionController.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Voicipher.Domain.InputModels.ControlPanel;
+using Voicipher.Domain.Interfaces.Commands;
+using Voicipher.Domain.Interfaces.Repositories;
+using Voicipher.Domain.OutputModels;
+using Voicipher.Domain.Payloads;
 using Voicipher.Host.Utils;
 
 namespace Voicipher.Host.Controllers.ControlPanel
@@ -14,22 +22,57 @@ namespace Voicipher.Host.Controllers.ControlPanel
     [ApiController]
     public class UserSubscriptionController : ControllerBase
     {
-        [HttpPost("create")]
-        public async Task<IActionResult> Create([FromForm] object createSubscriptionModel)
+        private readonly Lazy<IModifySubscriptionTimeCommand> _modifySubscriptionTimeCommand;
+        private readonly Lazy<ICurrentUserSubscriptionRepository> _currentUserSubscriptionRepository;
+        private readonly Lazy<IUserSubscriptionRepository> _userSubscriptionRepository;
+        private readonly Lazy<IMapper> _mapper;
+
+        public UserSubscriptionController(
+            Lazy<IModifySubscriptionTimeCommand> modifySubscriptionTimeCommand,
+            Lazy<ICurrentUserSubscriptionRepository> currentUserSubscriptionRepository,
+            Lazy<IUserSubscriptionRepository> userSubscriptionRepository,
+            Lazy<IMapper> mapper)
         {
-            throw new NotImplementedException();
+            _modifySubscriptionTimeCommand = modifySubscriptionTimeCommand;
+            _currentUserSubscriptionRepository = currentUserSubscriptionRepository;
+            _userSubscriptionRepository = userSubscriptionRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromForm] CreateSubscriptionInputModel createSubscriptionInputModel, CancellationToken cancellationToken)
+        {
+            var modifySubscriptionTimePayload = _mapper.Value.Map<ModifySubscriptionTimePayload>(createSubscriptionInputModel);
+            var commandResult = await _modifySubscriptionTimeCommand.Value.ExecuteAsync(modifySubscriptionTimePayload, HttpContext.User, cancellationToken);
+            if (!commandResult.IsSuccess)
+                return BadRequest(_mapper.Value.Map<ErrorResultOutputModel>(commandResult));
+
+            return Ok(commandResult.Value);
         }
 
         [HttpGet("{userId}")]
-        public async Task<IActionResult> GetSubscriptions(Guid userId)
+        public async Task<IActionResult> GetSubscriptions(Guid userId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var userSubscriptions = await _userSubscriptionRepository.Value.GetAllAsync(userId, cancellationToken);
+            var outputModels = userSubscriptions.Select(x => new
+            {
+                Id = x.Id,
+                UserId = x.UserId,
+                ApplicationId = x.ApplicationId,
+                Time = x.Time,
+                Operation = x.Operation,
+                DateCreatedUtc = x.DateCreatedUtc
+            });
+
+            return Ok(outputModels);
         }
 
         [HttpGet("remaining-time/{userId}")]
-        public async Task<IActionResult> GetSubscriptionRemainingTime(Guid userId)
+        public async Task<IActionResult> GetSubscriptionRemainingTime(Guid userId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var remainingTime = await _currentUserSubscriptionRepository.Value.GetRemainingTimeAsync(userId, cancellationToken);
+
+            return Ok(new TimeSpanWrapperOutputModel(remainingTime.Ticks));
         }
     }
 }
