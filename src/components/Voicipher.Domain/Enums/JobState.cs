4764e41d17997908b2388ef95ce5295d458ddec9
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-21 11:01:51 +0100

    Update job state.

--- src/components/Voicipher.Domain/Enums/JobState.cs
+++ src/components/Voicipher.Domain/Enums/JobState.cs
@@ -8,6 +8,8 @@
         Validated = 3,
         Converting = 4,
         Converted = 5,
+        Uploading = 6,
+        Uploaded = 7,
         Processing = 6,
         Processed = 7,
         Completed = 8
