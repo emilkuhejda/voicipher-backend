b4221b0a0cd9a02d750e1a4a313a1f2f41027368
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-26 17:13:52 +0100

    Create clean up controller.

---
+++ src/web/Voicipher.Host/Controllers/ControlPanel/CleanUpController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Voicipher.Host.Utils;
+
+namespace Voicipher.Host.Controllers.ControlPanel
+{
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/control-panel/clean-up")]
+    [Authorize(Policy = nameof(VoicipherPolicy.Admin))]
+    [ApiController]
+    [AllowAnonymous]
+    public class CleanUpController : ControllerBase
+    {
+        [HttpPost("audio-files")]
+        public async Task<IActionResult> CleanUpAudioFiles()
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+        [HttpPost("transcribe-item")]
+        public async Task<IActionResult> CleanUpTranscribeItems()
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+    }
+}
