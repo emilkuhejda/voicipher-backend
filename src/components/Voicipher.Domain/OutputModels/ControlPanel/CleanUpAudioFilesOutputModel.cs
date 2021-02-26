c21922e007f3447243d10e9142700c567af8a17e
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-26 22:44:48 +0100

    Create output model.

--- src/components/Voicipher.Domain/OutputModels/ControlPanel/CleanUpAudioFilesOutputModel.cs
+++ src/components/Voicipher.Domain/OutputModels/ControlPanel/CleanUpAudioFilesOutputModel.cs
@@ -1,6 +1,33 @@
-﻿namespace Voicipher.Domain.OutputModels.ControlPanel
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Voicipher.Domain.OutputModels.ControlPanel
 {
     public record CleanUpAudioFilesOutputModel
     {
+        public CleanUpAudioFilesOutputModel()
+            : this(0, new Dictionary<Guid, Guid[]>(), new Dictionary<Guid, Guid[]>())
+        {
+        }
+
+        public CleanUpAudioFilesOutputModel(int audioFilesToBackup, Dictionary<Guid, Guid[]> succeededIds, Dictionary<Guid, Guid[]> failedIds)
+        {
+            AudioFilesToBackup = audioFilesToBackup;
+            SucceededTotal = succeededIds.Sum(x => x.Value.Length);
+            FailedTotal = failedIds.Sum(x => x.Value.Length);
+            SucceededIds = succeededIds;
+            FailedIds = failedIds;
+        }
+
+        public int AudioFilesToBackup { get; }
+
+        public int SucceededTotal { get; }
+
+        public int FailedTotal { get; }
+
+        public Dictionary<Guid, Guid[]> SucceededIds { get; }
+
+        public Dictionary<Guid, Guid[]> FailedIds { get; }
     }
 }
