a86f7aaa6f48ca56f282258396c0709c848e21dd
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-27 09:07:29 +0100

    Get all files.

--- src/components/Voicipher.Business/Commands/ControlPanel/CleanUpBaseCommand.cs
+++ src/components/Voicipher.Business/Commands/ControlPanel/CleanUpBaseCommand.cs
@@ -1,6 +1,19 @@
-﻿namespace Voicipher.Business.Commands.ControlPanel
+﻿using Serilog;
+using Voicipher.Business.Infrastructure;
+using Voicipher.Domain.Interfaces.Repositories;
+
+namespace Voicipher.Business.Commands.ControlPanel
 {
-    public abstract class CleanUpBaseCommand
+    public abstract class CleanUpBaseCommand<T, TResult> : Command<T, TResult>
     {
+        protected CleanUpBaseCommand(IAudioFileRepository audioFileRepository, ILogger logger)
+        {
+            AudioFileRepository = audioFileRepository;
+            Logger = logger;
+        }
+
+        protected IAudioFileRepository AudioFileRepository { get; }
+
+        protected ILogger Logger { get; }
     }
 }
--- src/components/Voicipher.Business/Commands/ControlPanel/CleanUpTranscribeItemsCommand.cs
+++ src/components/Voicipher.Business/Commands/ControlPanel/CleanUpTranscribeItemsCommand.cs
@@ -1,18 +1,35 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
-using Voicipher.Business.Infrastructure;
+using Serilog;
 using Voicipher.Domain.Infrastructure;
 using Voicipher.Domain.Interfaces.Commands.ControlPanel;
+using Voicipher.Domain.Interfaces.Repositories;
 using Voicipher.Domain.OutputModels.ControlPanel;
 using Voicipher.Domain.Payloads.ControlPanel;
 
 namespace Voicipher.Business.Commands.ControlPanel
 {
-    public class CleanUpTranscribeItemsCommand : Command<CleanUpTranscribeItemsPayload, CommandResult<CleanUpTranscribeItemsOutputModel>>, ICleanUpTranscribeItemsCommand
+    public class CleanUpTranscribeItemsCommand : CleanUpBaseCommand<CleanUpTranscribeItemsPayload, CommandResult<CleanUpTranscribeItemsOutputModel>>, ICleanUpTranscribeItemsCommand
     {
+        public CleanUpTranscribeItemsCommand(IAudioFileRepository audioFileRepository, ILogger logger)
+            : base(audioFileRepository, logger.ForContext<CleanUpTranscribeItemsCommand>())
+        {
+        }
+
         protected override async Task<CommandResult<CleanUpTranscribeItemsOutputModel>> Execute(CleanUpTranscribeItemsPayload parameter, ClaimsPrincipal principal, CancellationToken cancellationToken)
         {
+            var deleteBefore = DateTime.UtcNow.AddDays(0);
+            var audioFiles = await AudioFileRepository.GetAllForCleanUpAsync(deleteBefore, cancellationToken);
+            if (!audioFiles.Any())
+            {
+                Logger.Information("No audio files for cleanup");
+                return new CommandResult<CleanUpTranscribeItemsOutputModel>(new CleanUpTranscribeItemsOutputModel());
+            }
+
+            return new CommandResult<CleanUpTranscribeItemsOutputModel>(new CleanUpTranscribeItemsOutputModel());
         }
     }
 }
