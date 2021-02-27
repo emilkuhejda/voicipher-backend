61c5f3103f0b461cd6b88fa81d6fbbe6cfca880b
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-02-27 13:28:23 +0100

    Add new migration.

---
+++ src/components/Voicipher.DataAccess/Migrations/20210227122814_Release01.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Voicipher.DataAccess.Migrations
+{
+    public partial class Release01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "WasCleaned",
+                table: "TranscribeItem",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Exception",
+                table: "BackgroundJob",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WasCleaned",
+                table: "TranscribeItem");
+
+            migrationBuilder.DropColumn(
+                name: "Exception",
+                table: "BackgroundJob");
+        }
+    }
+}
