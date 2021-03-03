cb59b277b7c36e62a874d7556c6a1f99777d59d9
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-03 17:12:34 +0100

    Remove deleting invalid speech results. (#95)

--- src/components/Voicipher.Business/Extensions/TranscribeItemExtensions.cs
+++ src/components/Voicipher.Business/Extensions/TranscribeItemExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Newtonsoft.Json;
 using Voicipher.Domain.Models;
 
@@ -10,25 +9,6 @@ namespace Voicipher.Business.Extensions
         public static IEnumerable<RecognitionAlternative> GetAlternatives(this TranscribeItem transcribeItem)
         {
             return JsonConvert.DeserializeObject<IEnumerable<RecognitionAlternative>>(transcribeItem.Alternatives);
-        }
-
-        public static IEnumerable<RecognitionAlternative> RemoveInvalidAlternatives(this IEnumerable<RecognitionAlternative> recognitionAlternatives)
-        {
-            var alternative = recognitionAlternatives
-                .GroupBy(x => x.ResultNumber)
-                .ToDictionary(x => x.Key, y =>
-                {
-                    var alternatives = y.ToList();
-                    return new
-                    {
-                        Confidence = alternatives.Any() ? alternatives.Sum(x => x.Confidence) / alternatives.Count : 0,
-                        Alternatives = alternatives
-                    };
-                })
-                .OrderByDescending(x => x.Value.Confidence)
-                .FirstOrDefault();
-
-            return alternative.Value?.Alternatives ?? Enumerable.Empty<RecognitionAlternative>();
         }
     }
 }
