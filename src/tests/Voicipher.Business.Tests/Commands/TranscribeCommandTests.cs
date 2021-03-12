ea434057804e4f8d643d9de9e25de5ede02e6792
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-12 16:14:32 +0100

    Unit testing.

--- src/tests/Voicipher.Business.Tests/Commands/TranscribeCommandTests.cs
+++ src/tests/Voicipher.Business.Tests/Commands/TranscribeCommandTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using Serilog;
 using Voicipher.Business.Commands.Audio;
 using Voicipher.Domain.Enums;
+using Voicipher.Domain.Exceptions;
 using Voicipher.Domain.Infrastructure;
 using Voicipher.Domain.Interfaces.Channels;
 using Voicipher.Domain.Interfaces.Commands.Transcription;
@@ -132,6 +133,102 @@ namespace Voicipher.Business.Tests.Commands
 
             audioFileProcessingChannelMock
                 .Verify(x => x.AddFileAsync(It.Is<RecognitionFile>(r => r.AudioFileId == audioFile.Id), default));
+        }
+
+        [Fact]
+        public async Task ExecuteCommand_UnsupportedLanguage_ThrowsException()
+        {
+            // Arrange
+            var canRunRecognitionCommandMock = new Mock<ICanRunRecognitionCommand>();
+            var audioFileProcessingChannelMock = new Mock<IAudioFileProcessingChannel>();
+            var audioFileRepositoryMock = new Mock<IAudioFileRepository>();
+            var backgroundJobRepositoryMock = new Mock<IBackgroundJobRepository>();
+            var loggerMock = new Mock<ILogger>();
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+            }));
+
+            var audioFile = new AudioFile
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                UploadStatus = UploadStatus.Completed,
+                RecognitionState = RecognitionState.None,
+                TotalTime = TimeSpan.FromMinutes(10)
+            };
+
+            canRunRecognitionCommandMock
+                .Setup(x => x.ExecuteAsync(It.IsAny<CanRunRecognitionPayload>(), It.Is<ClaimsPrincipal>(c => c == claimsPrincipal), default))
+                .ReturnsAsync(new CommandResult());
+            audioFileRepositoryMock
+                .Setup(x => x.GetAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), default))
+                .ReturnsAsync(audioFile);
+            loggerMock.Setup(x => x.ForContext<It.IsAnyType>()).Returns(Mock.Of<ILogger>());
+
+            var transcribeCommand = new TranscribeCommand(
+                canRunRecognitionCommandMock.Object,
+                audioFileProcessingChannelMock.Object,
+                audioFileRepositoryMock.Object,
+                backgroundJobRepositoryMock.Object,
+                loggerMock.Object);
+
+            var transcribePayload = new TranscribePayload(audioFile.Id, "lang", 0, 0, Guid.NewGuid());
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<OperationErrorException>(async () => await transcribeCommand.ExecuteAsync(transcribePayload, claimsPrincipal, default));
+
+            // Assert
+            Assert.Equal(ErrorCode.EC200, exception.ErrorCode);
+        }
+
+        [Fact]
+        public async Task ExecuteCommand_InvalidEndTime_ThrowsException()
+        {
+            // Arrange
+            var canRunRecognitionCommandMock = new Mock<ICanRunRecognitionCommand>();
+            var audioFileProcessingChannelMock = new Mock<IAudioFileProcessingChannel>();
+            var audioFileRepositoryMock = new Mock<IAudioFileRepository>();
+            var backgroundJobRepositoryMock = new Mock<IBackgroundJobRepository>();
+            var loggerMock = new Mock<ILogger>();
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+            }));
+
+            var audioFile = new AudioFile
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                UploadStatus = UploadStatus.Completed,
+                RecognitionState = RecognitionState.None,
+                TotalTime = TimeSpan.FromMinutes(10)
+            };
+
+            canRunRecognitionCommandMock
+                .Setup(x => x.ExecuteAsync(It.IsAny<CanRunRecognitionPayload>(), It.Is<ClaimsPrincipal>(c => c == claimsPrincipal), default))
+                .ReturnsAsync(new CommandResult());
+            audioFileRepositoryMock
+                .Setup(x => x.GetAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), default))
+                .ReturnsAsync(audioFile);
+            loggerMock.Setup(x => x.ForContext<It.IsAnyType>()).Returns(Mock.Of<ILogger>());
+
+            var transcribeCommand = new TranscribeCommand(
+                canRunRecognitionCommandMock.Object,
+                audioFileProcessingChannelMock.Object,
+                audioFileRepositoryMock.Object,
+                backgroundJobRepositoryMock.Object,
+                loggerMock.Object);
+
+            var transcribePayload = new TranscribePayload(audioFile.Id, "en-US", 10, 0, Guid.NewGuid());
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<OperationErrorException>(async () => await transcribeCommand.ExecuteAsync(transcribePayload, claimsPrincipal, default));
+
+            // Assert
+            Assert.Equal(ErrorCode.EC600, exception.ErrorCode);
         }
     }
 }
