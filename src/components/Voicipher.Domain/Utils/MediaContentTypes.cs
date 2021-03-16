f6c37181767209c95c24c9064f897b34ff1689dc
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-16 15:38:19 +0100

    Update checking unsupported types. (#108)

--- src/components/Voicipher.Domain/Utils/MediaContentTypes.cs
+++ src/components/Voicipher.Domain/Utils/MediaContentTypes.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Voicipher.Domain.Utils
 {
     public static class MediaContentTypes
     {
-        private static readonly HashSet<string> Mp3ContentTypes = new(StringComparer.OrdinalIgnoreCase)
-        {
-            "video/mpeg",
-            "video/x-mpeg"
-        };
-
         public static bool IsUnsupported(string contentType)
         {
-            return !contentType.Contains("audio", StringComparison.OrdinalIgnoreCase) || Mp3ContentTypes.Contains(contentType);
+            return !contentType.Contains("audio", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
